ab730b4015e4b279cce2efc020a84e02a5847804
GeonwooVincentKim <kdsnop@gmail.com>
2021-01-22 17:39:58 +0900

    Successfully viewed all of list

---
+++ js/sub_page/albums.js
@@ -0,0 +1,32 @@
+function request_album(detail_dir){
+    console.log("Testing..");
+    fetch("https://jsonplaceholder.typicode.com/" + detail_dir)
+    .then(function(response){
+        return response.json()
+    }).then((response) => {
+        var result = document.getElementById('result')
+        var item = ''
+        console.log(response);
+        // response.for (index = 0; index < array.length, index++) {
+        //     element = array[index];
+        //     item = 'User ID: ' + element.userId + ' ID: ' + element.id + " title: " + element.title + " - completed: " + element.completed + "<br><br>";
+        //     console.log(item);
+        //     result.append(item);
+        // }(element=>{
+            
+        //     console.log(item);
+        //     result.append(item);
+        // });
+        // response.forEach(element =>{
+        //     item = 'User ID: ' + element.userId + ' ID: ' + element.id + "\n"
+        //     console.log(item);
+        //     result.append(item);
+        // });
+        response.forEach(element=>{
+            item = 'User ID: ' + element.userId + ' ID: ' + element.id + " title: " + element.title
+            console.log(item);
+            var n = "\n";
+            result.append(item + "\n");
+        });
+    })
+}
