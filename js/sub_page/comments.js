eca8e13f0acac5ad72b759bafa1d85be89e8b2d9
GeonwooVincentKim <kdsnop@gmail.com>
2021-01-26 16:23:12 +0900

    Trying to solve the detail-page of project

--- js/sub_page/comments.js
+++ js/sub_page/comments.js
@@ -1,3 +1,6 @@
+var comments_con = 0;
+var result = document.getElementById('result_post')
+
 function request_comments(detail_dir){
     console.log("Testing..");
     fetch("https://jsonplaceholder.typicode.com/" + detail_dir)
@@ -26,7 +29,23 @@ function request_comments(detail_dir){
             item = 'Post ID: ' + element.postId + ' ID: ' + element.id + " name: " + element.title + " email: " + element.email + " body: " + element.body
             console.log(item); 
             var n = "\n";
-            result.append(item + "\n");
+            // result.append(item + "\n");
+            result.innerHTML += `
+                <div class="result_list">
+                    <div class="list_body">
+                        <a href="/sub_page/comments.html?userId=${response[comments_con].id}&Id=${response[comments_con].postId}">
+                            ${response[comments_con].postId}
+                            ${response[comments_con].id}
+                            ${response[comments_con].title}
+                            ${response[comments_con].email}
+                            ${response[comments_con].body}
+                        </a>
+                    </div>
+                </div>
+            `
+            comments_con += 1;
+            console.log(result);
+            return result;
         });
     })
 }
