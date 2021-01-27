4a9855e883ca8d5dcd3ea4d5273d4d885b6fb369
GeonwooVincentKim <kdsnop@gmail.com>
2021-01-27 17:16:40 +0900

    Cannot get the comments data that below the detail-posts

--- js/sub_page/posts.js
+++ js/sub_page/posts.js
@@ -10,25 +10,9 @@ function request_post(detail_dir){
         var result = document.getElementById('result_post')
         var item = ''
         console.log(response);
-        // response.for (index = 0; index < array.length, index++) {
-        //     element = array[index];
-        //     item = 'User ID: ' + element.userId + ' ID: ' + element.id + " title: " + element.title + " - completed: " + element.completed + "<br><br>";
-        //     console.log(item);
-        //     result.append(item);
-        // }(element=>{
-        //     console.log(item);
-        //     result.append(item);
-        // });
-        // response.forEach(element =>{
-        //     item = 'User ID: ' + element.userId + ' ID: ' + element.id + "\n"
-        //     console.log(item);
-        //     result.append(item);
-        // });
         response.forEach(element=>{
             item = 'User ID: ' + element.userId + ' ID: ' + element.id + " title: " + element.title + " Body: " + element.body
             console.log(item);
-            var n = "\n";
-            // result.append(item + "\n");
             result.innerHTML += `
                 <div class="result_list">
                     <div class="list_body">
@@ -59,7 +43,8 @@ function request_post_detail(detail_info){
     //         console.log(tmp[1]);
     //     }
     // }
-
+    
+    /* Display sub-directory */
     location.search
         .substr(1)
         .split("&")
@@ -70,6 +55,11 @@ function request_post_detail(detail_info){
                 console.log(tmp[0]);
                 console.log(tmp[1]);
             }
+            // if(tmp[1] === detail_info){
+            //     result = decodeURIComponent(tmp[2]);
+            //     console.log(tmp[1]);
+            //     console.log(tmp[2]);
+            // }
         });
 
     fetch("https://jsonplaceholder.typicode.com/posts?id=" + result)
@@ -90,10 +80,138 @@ function request_post_detail(detail_info){
                         Name: ${element.id}
                         Title: ${element.title}
                         Body: ${element.body}
-                    </div>
+                    </p>
                 </div>
             `;
+            // result = document.getElementById("result_post_comment")
+            // console.log(result);
+            // response.forEach(element=>{
+            //     item = 'Post ID: ' + element.postId + ' ID: ' + element.id + " name: " + element.title + " email: " + element.email + " body: " + element.body
+            //     console.log(item);
+            //     result.innerHTML += `
+            //         <div class="result_list">
+            //             <div class="list_body">
+            //                 <a href="/sub_page/comments.html?userId=${response[comments_con].id}&Id=${response[comments_con].postId}">
+            //                     "Post-id: ${response[comments_con].postId}
+            //                     ${response[comments_con].id}
+            //                     ${response[comments_con].title}
+            //                     ${response[comments_con].email}
+            //                     ${response[comments_con].body}
+            //                 </a>
+            //             </div>
+            //         </div>
+            //     `
+                // comments_con += 1;
+            // })
         })
         return result;
+    })
+
+    // fetch("https://jsonplaceholder.typicode.com/posts?id=" + result + "/" + result)
+    // .then(function(response){
+    //     return response.json()
+    // }).then((response) => {
+    //     result = document.getElementById('result_post_detail')
+    //     console.log(response);
+    //     // response.forEach(element=>{
+    //     //     item = 'User ID: ' + element.userId + ' ID: ' + element.id + " title: " + element.title + " Body: " + element.body
+    //     //     console.log(item);
+    //     //     var n = "\n";
+    //     //     // result.append(item + "\n");
+    //     //     result.innerHTML += `
+    //     //         <div class="result_list_detail">
+    //     //             <p class="detail_list_body">
+    //     //                 ID: ${element.userId}
+    //     //                 Name: ${element.id}
+    //     //                 Title: ${element.title}
+    //     //                 Body: ${element.body}
+    //     //             </div>
+    //     //         </div>
+    //     //     `;
+    //     // })
+    //     response.forEach(element=>{
+    //         item = 'Post ID: ' + element.postId + ' ID: ' + element.id + " name: " + element.title + " email: " + element.email + " body: " + element.body
+    //         console.log(item); 
+    //         var n = "\n";
+    //         // result.append(item + "\n");
+    //         result.innerHTML += `
+    //             <div class="result_list">
+    //                 <div class="list_body">
+    //                     <a href="/sub_page/comments.html?userId=${response[comments_con].id}&Id=${response[comments_con].postId}">
+    //                         ${response[comments_con].postId}
+    //                         ${response[comments_con].id}
+    //                         ${response[comments_con].title}
+    //                         ${response[comments_con].email}
+    //                         ${response[comments_con].body}
+    //                     </a>
+    //                 </div>
+    //             </div>
+    //         `
+    //         comments_con += 1;
+    //         console.log(response[comments_con].body);
+    //         console.log(result);
+    //         return result;
+    //     });
+    //     return result;
+    // })
+}
+
+function request_comment_detail(detail_comments){
+    // var result = null, tmp = [];
+    // location.search
+    //     .substr(1)
+    //     .split("&")
+    //     .forEach(function(item){
+    //         tmp = item.split("=");
+    //         if(tmp[0] === detail_comments){
+    //             result = decodeURIComponent(tmp[1]);
+    //             console.log(tmp[0]);
+    //             console.log(tmp[1]);
+    //         }
+    //     });
+    var result = null, tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function(item){
+            tmp = item.split("=");
+            if(tmp[0] === detail_comments) {
+                result = decodeURIComponent(tmp[1]);
+                console.log(tmp[0]);
+                console.log(tmp[1]);
+            }
+            // if(tmp[1] === detail_info){
+            //     result = decodeURIComponent(tmp[2]);
+            //     console.log(tmp[1]);
+            //     console.log(tmp[2]);
+            // }
+        });
+    fetch("https://jsonplaceholder.typicode.com/posts?id=" + result + "/" + detail_comments)
+    .then(function(response){
+        return response.json()
+    }).then((response) => {
+        result = document.getElementById('result_post_comment')
+        console.log(response + "Should be here");
+        console.log(detail_comments);
+        response.forEach(element=>{
+            item = 'Post ID: ' + element.postId + ' ID: ' + element.id + " name: " + element.title + " email: " + element.email + " body: " + element.body
+            console.log(item); 
+            var n = "\n";
+            // result.append(item + "\n");
+            result.innerHTML += `
+                <div class="result_list">
+                    <div class="list_body">
+                        ${response[comments_con].postId}
+                        ${response[comments_con].id}
+                        ${response[comments_con].title}
+                        ${response[comments_con].email}
+                        ${response[comments_con].body}
+                    </div>
+                </div>
+            `
+            comments_con += 1;
+            console.log(result);
+            return result;
+        });
     })
 }
